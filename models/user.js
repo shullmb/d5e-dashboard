de7048d3eab0c3f5115fa49091af2609984fd193
FX-Wood <fxwood2@gmail.com>
2019-03-19 15:48:39 -0700

    Created user model and auth methods

---
+++ models/user.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'You must enter a name'],
+        minlength: [1, 'Your name must be between 1 and 99 characters'],
+        maxlength: [99, 'Your name must be between 1 and 99 characters']
+    },
+    password: {
+        type: String,
+        required: [true, 'You must enter a password'],
+        minlength: [10, 'Your password must be between 10 and 128 characters'],
+        maxlength: [128, 'Your password must be between 10 and 128 characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'You must enter an email'],
+        minlength: [6, 'Your email must be between 5 and 99 characters'],
+        maxlength: [128, 'Your email must be between 1 and 99 characters'],
+        validate: {
+            validator: function(v) {
+                return /.+@.+\..+/.test(v)
+            },
+            message: props => `${props.value} is not a valid email`
+        }
+    }
+    
+})
+
+// helper function to strip secrets from the user instance
+userSchema.set('toObject', {
+    transform: (doc, ret, options) => {
+        let returnJson = {
+            _id: ret._id,
+            email: ret.email,
+            name: ret.name
+        }
+        return returnJson;
+    }
+})
+
+// compares passwords
+userSchema.methods.authenticated = (password) => {
+    return bcrypt.compareSync(password, this.password);
+}
+
+// hashes passwords before saving to the database
+userSchema.pre('save', next => {
+    if (this.isNew) {
+        let hash = bcrypt.hashSync(this.password, 12);
+        this.password = hash;
+    }
+    next();
+})
+
+module.exports = mongoose.model('User', userSchema);
