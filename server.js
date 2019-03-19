9c9b02baa5cc81d107b3e4176ab9d8d67185cd80
FX-Wood <fxwood2@gmail.com>
2019-03-19 15:17:09 -0700

    Adding server boilerplate and config

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const expressJWT = require('express-JWT');
+const RateLimit = require('express-rate-limit');
+
+
+const app = express();
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost/jwt');
+const db = mongoose.connection;
+db.on('open', () => {
+    console.log(`Connected to Mongo on ${db.host}: ${db.port}`)
+})
+db.on('error', (err) => {
+    console.log(`Database error:\n${err}`)
+})
+
+app.get('/', (req, res) => {
+    res.send("You hit the root route");
+})
+
+
+app.listen(process.env.EXPRESS_PORT, () => {
+    console.log(`You're listening to the sweet sounds of ${process.env.EXPRESS_PORT} jwt-authorization in the morning...`)
+    console.log(`Oh, and the port is`, EXPRESS_PORT)
+})
