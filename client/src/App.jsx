16352fb9ddf9d29180ba3bf129a4355e4699e542
FX-Wood <fxwood2@gmail.com>
2019-03-20 15:29:11 -0700

    Adding hook to check for existing tokens

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Signup from './Signup';
 import Login from './Login';
 import UserProfile from './UserProfile';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -11,11 +12,53 @@ class App extends Component {
       token: '',
       user: null,
       email: '',
+      errorMessage: '',
     }
     this.liftTokenToState = this.liftTokenToState.bind(this)
+    this.checkForLocalToken = this.checkForLocalToken.bind(this)
   }
+
   liftTokenToState({token, user}) {
     this.setState({token, user})
+  }
+
+  checkForLocalToken() {
+    let token = localStorage.getItem('jwtToken')
+    if (!token || token === 'undefined') {
+      // If there is no token, remove the entry in localStorage
+      localStorage.removeItem('jwtToken')
+      this.setState({
+        token: '',
+        user: null
+      })
+    } else {
+      // If found, send token to be verified
+      axios.post('/auth/me/from/token')
+      .then( res => {
+        if (res.data.type === 'error') {
+          console.log('error', res.data)
+          // if error, remove the bad token and display an error
+          localStorage.removeItem('jwtToken')
+          this.setState({
+            errorMessage: res.data.message
+          })
+        } else {
+          // Upon receipt, store token 
+          localStorage.setItem('jwtToken', res.data.token)
+          // Put token in state
+          this.setState({
+            token: res.data.token,
+            user: res.data.user
+          })
+        }
+        console.log(res)
+      }).catch( err => {
+        console.log(err)
+      })
+    }
+  }
+  componentDidMount() {
+    this.checkForLocalToken()
   }
   render() {
     return (
