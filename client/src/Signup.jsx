7ff82a995ecc6ae470ed783dbeba8bb0f0c35f66
FX-Wood <fxwood2@gmail.com>
2019-03-20 15:13:59 -0700

    Touching up signup

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -13,6 +13,7 @@ export default class Signup extends Component {
         this.handleNameChange = this.handleNameChange.bind(this)
         this.handleEmailChange = this.handleEmailChange.bind(this)
         this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleNameChange(e) {
@@ -31,11 +32,14 @@ export default class Signup extends Component {
         })
     }
     handleSubmit(e) {
+        console.log('signing up...')
+        e.preventDefault()
         axios.post('/auth/signup', {
             name: this.state.name,
             email: this.state.email,
             password: this.state.password
         }).then( res => {
+            console.log('res.data', res.data)
             if (res.data.type === 'error') {
                 console.log('error', res.data)
             } else {
@@ -44,6 +48,8 @@ export default class Signup extends Component {
                 this.props.liftToken(res.data)
 
             }
+        }).catch(err => {
+            console.log(err)
         })
     }
     render() {
@@ -54,6 +60,7 @@ export default class Signup extends Component {
                     <input onChange={this.handleNameChange} value={this.state.name} type="text" name="name" placeholder="Enter your full name"/>
                     <input onChange={this.handleEmailChange} value={this.state.email} type="email" name="email" placeholder="Enter your email address"/>
                     <input onChange={this.handlePasswordChange} value={this.state.password} type="password" name="password" placeholder="Choose a password..."/>
+                    <input type="submit" value="Sign Up!"/>
                 </form>
             </div>
         )
